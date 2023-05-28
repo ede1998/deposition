ad43ebb62a82f8e509f4c8ed1d923927a95c3bbe
ede1998 <online@erik-hennig.me>
2023-05-28 21:19:53 +0200

    working distance displaying

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,28 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::fmt::Write;
 use embassy_executor::Executor;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
 use embassy_time::{Duration, Timer};
+use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::{Alignment, Text};
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{
     adc::{AdcConfig, Attenuation, ADC, ADC2},
     analog::AvailableAnalog,
     clock::ClockControl,
+    i2c::I2C,
     peripherals::Peripherals,
     prelude::*,
     timer::TimerGroup,
     Rtc, IO,
 };
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use static_cell::StaticCell;
 
 async fn poll<T, E>(mut f: impl FnMut() -> nb::Result<T, E>) -> Result<T, E> {
@@ -41,7 +49,7 @@ const FIX_POINTS: [Mapping; 5] = [
 struct Millimeters(u16);
 
 impl Millimeters {
-    pub fn from_adc_reading_simple(reading: u16) -> Self {
+    pub fn _from_adc_reading_simple(reading: u16) -> Self {
         const FACTOR: u64 = 256;
         const SLOPE: u64 = (0.185185185185185 * FACTOR as f64) as _;
         const OFFSET: u64 = (90.2962962962963 * FACTOR as f64) as _;
@@ -80,15 +88,18 @@ impl Millimeters {
             }
         }
     }
+
+    pub fn as_cm(self) -> u16 {
+        self.0 / 10
+    }
 }
 
 static HEIGHT: Signal<CriticalSectionRawMutex, Millimeters> = Signal::new();
 
 #[embassy_executor::task]
-async fn measure(io: IO, analog: AvailableAnalog) {
+async fn measure(gpio25: hal::gpio::Gpio25<hal::gpio::Unknown>, analog: AvailableAnalog) {
     let mut adc2_config = AdcConfig::new();
-    let mut pin25 =
-        adc2_config.enable_pin(io.pins.gpio25.into_analog(), Attenuation::Attenuation11dB);
+    let mut pin25 = adc2_config.enable_pin(gpio25.into_analog(), Attenuation::Attenuation11dB);
     let mut adc2 = ADC::<ADC2>::adc(analog.adc2, adc2_config).unwrap();
 
     loop {
@@ -100,86 +111,39 @@ async fn measure(io: IO, analog: AvailableAnalog) {
     }
 }
 
-//#[embassy_executor::task]
-//async fn display() {
-//    // Create a new peripheral object with the described wiring
-//    // and standard I2C clock speed
-//    let i2c = I2C::new(
-//        peripherals.I2C0,
-//        io.pins.gpio32,
-//        io.pins.gpio27,
-//        100u32.kHz(),
-//        &mut system.peripheral_clock_control,
-//        &clocks,
-//    );
-//
-//    // Start timer (5 second interval)
-//    timer0.start(5u64.secs());
-//
-//    // Initialize display
-//    let interface = I2CDisplayInterface::new(i2c);
-//    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-//        .into_buffered_graphics_mode();
-//    display.init().unwrap();
-//
-//    // Specify different text styles
-//    let text_style = MonoTextStyleBuilder::new()
-//        .font(&FONT_6X10)
-//        .text_color(BinaryColor::On)
-//        .build();
-//    let text_style_big = MonoTextStyleBuilder::new()
-//        .font(&FONT_9X18_BOLD)
-//        .text_color(BinaryColor::On)
-//        .build();
-//
-//    loop {
-//        // Fill display buffer with a centered text with two lines (and two text
-//        // styles)
-//        Text::with_alignment(
-//            "esp-hal",
-//            display.bounding_box().center() + Point::new(0, 0),
-//            text_style_big,
-//            Alignment::Center,
-//        )
-//        .draw(&mut display)
-//        .unwrap();
-//
-//        Text::with_alignment(
-//            "Chip: ESP32",
-//            display.bounding_box().center() + Point::new(0, 14),
-//            text_style,
-//            Alignment::Center,
-//        )
-//        .draw(&mut display)
-//        .unwrap();
-//
-//        // Write buffer to display
-//        display.flush().unwrap();
-//        // Clear display buffer
-//        display.clear();
-//
-//        // Wait 5 seconds
-//        block!(timer0.wait()).unwrap();
-//
-//        // Write single-line centered text "Hello World" to buffer
-//        Text::with_alignment(
-//            "Hello World!",
-//            display.bounding_box().center(),
-//            text_style_big,
-//            Alignment::Center,
-//        )
-//        .draw(&mut display)
-//        .unwrap();
-//
-//        // Write buffer to display
-//        display.flush().unwrap();
-//        // Clear display buffer
-//        display.clear();
-//
-//        // Wait 5 seconds
-//        block!(timer0.wait()).unwrap();
-//    }
-//}
+#[embassy_executor::task]
+async fn display(i2c: I2C<'static, hal::peripherals::I2C0>) {
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    // Specify different text styles
+    let text_style_big = MonoTextStyleBuilder::new()
+        .font(&FONT_10X20)
+        .text_color(BinaryColor::On)
+        .build();
+
+    loop {
+        let height = HEIGHT.wait().await;
+        let mut string = heapless::String::<10>::new();
+        write!(&mut string, "{}cm", height.as_cm()).unwrap();
+        
+        Text::with_alignment(
+            &string,
+            display.bounding_box().center(),
+            text_style_big,
+            Alignment::Center,
+        )
+        .draw(&mut display)
+        .unwrap();
+
+        display.flush().unwrap();
+        display.clear();
+
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
 
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
@@ -213,10 +177,21 @@ fn main() -> ! {
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let analog = peripherals.SENS.split();
+    // Create a new peripheral object with the described wiring
+    // and standard I2C clock speed
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio32,
+        io.pins.gpio27,
+        100u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
 
     let executor = EXECUTOR.init(Executor::new());
     executor.run(|spawner| {
-        spawner.spawn(measure(io, analog)).ok();
+        spawner.spawn(measure(io.pins.gpio25, analog)).unwrap();
+        spawner.spawn(display(i2c)).unwrap();
     });
 }
 
